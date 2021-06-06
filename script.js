df5c90ff1ef4d5f0ef33108891ac486fba466c53
Anjeh Mbah <junez24@yahoo.com>
2021-06-06 19:00:30 -0400

    initial html css and js beginning work

---
+++ script.js
@@ -0,0 +1,5 @@
+var inputValue = document.querySelector('.inputValue')
+var submitBtn = document.querySelector('.btn')
+var specification = document.querySelector('.description');
+var name = document.querySelector('.appname');
+var condition = document.querySelector('.temperature');
